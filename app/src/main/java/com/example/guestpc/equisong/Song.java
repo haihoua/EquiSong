57a5109b9ce43f8500e0937b6df68e2684da0577
unknown <yangh1@CS336-258.appd.appstate.edu>
2014-09-23 16:08:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/guestpc/equisong/Song.java
@@ -0,0 +1,23 @@
+package com.example.guestpc.equisong;
+
+/**
+ * Created by GuestPC on 9/18/2014.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+
+}
